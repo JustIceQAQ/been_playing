6efe9567af9c54b843232ff2dff63a0b9cd72406
justiceqaq <justiceqaq@gmail.com>
2022-04-28 02:00:50 +0800

    tweak: RequestsClean replace_string_set

--- helper/clean_helper.py
+++ helper/clean_helper.py
@@ -10,7 +10,8 @@ class CleanInit(metaclass=ABCMeta):
 class RequestsClean(CleanInit):
     @staticmethod
     def clean_string(raw_string: str) -> str:
-        replace_string_set = {"\u3000", "\n"}
+        # TODO: big5 utf-8
+        replace_string_set = {"\u3000", "\n", "\u56fd"}
         for replace_string in replace_string_set:
             raw_string = raw_string.replace(replace_string, " ")
         return ' '.join(raw_string.split()).strip()
