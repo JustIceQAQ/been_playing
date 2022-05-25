f6d87ac8bab7abe819d4cac850fedde6b38bac07
justiceqaq <justiceqaq@gmail.com>
2022-05-26 04:45:26 +0800

    add: run_all_script.py

---
+++ run_all_script.py
@@ -0,0 +1,24 @@
+import threading
+from pathlib import Path
+
+from exhibition.cksmh_script import cksmh_script
+from exhibition.huashan1914_script import huashan1914_script
+from exhibition.mocataipei_script import mocataipei_script
+from exhibition.npm_script import npm_script
+
+ROOT_DIR = Path(__file__).resolve(strict=True).parent
+
+if __name__ == '__main__':
+    py_scripts = {
+        cksmh_script,
+        huashan1914_script,
+        mocataipei_script,
+        npm_script,
+    }
+    runners = [
+        threading.Thread(target=py_script) for py_script in py_scripts
+    ]
+    for runner in runners:
+        runner.start()
+    for runner in runners:
+        runner.join()
