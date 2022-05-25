d069e5119fb33b83fcd6e0f7e5800802e843fa11
justiceqaq <justiceqaq@gmail.com>
2022-05-26 02:53:39 +0800

    add: add ParseInit and Parse classes

---
+++ helper/parse_helper.py
@@ -0,0 +1,141 @@
+import inspect
+from abc import ABCMeta, abstractmethod
+
+import bs4
+
+
+class ParseInit(metaclass=ABCMeta):
+    @abstractmethod
+    def get_title(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_date(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_address(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_figure(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_source_url(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def parsed(self, *args, **kwargs):
+        methods = [
+            method for method in inspect.getmembers(self, predicate=inspect.ismethod)
+            if method[0].startswith('get_')
+        ]
+        return {
+            def_name.split("get_")[-1]: method(*args, **kwargs)
+            for def_name, method in methods
+        }
+
+
+class MocaTaipeiParse(ParseInit):
+    def __init__(self, item: bs4.element.Tag):
+        self.item = item
+
+    def get_title(self, *args, **kwargs) -> str:
+        return self.item.find("h3", {"class": "imgTitle"}).get_text()
+
+    def get_date(self, *args, **kwargs):
+        return self.item.find("div", {"class": "dateBox"}).get_text()
+
+    def get_address(self, *args, **kwargs):
+        return "-"
+
+    def get_figure(self, *args, **kwargs):
+        target_domain = kwargs.get("target_domain", None)
+        if target_domain is None:
+            raise ValueError("請提供 TARGET_DOMAIN")
+        return "{}{}".format(target_domain, self.item.select_one("figure.imgFrame img")["data-src"])
+
+    def get_source_url(self, *args, **kwargs):
+        return self.item.select_one("a.textFrame")["href"]
+
+
+class NpmRowParse(ParseInit):
+    def __init__(self, item: bs4.element.Tag):
+        self.item = item
+
+    def get_title(self, *args, **kwargs) -> str:
+        return self.item.find("h3", {"class": "font-medium"}).get_text()
+
+    def get_date(self, *args, **kwargs) -> str:
+        return self.item.find("div", {"class": "exhibition-list-date"}).get_text()
+
+    def get_address(self, *args, **kwargs) -> str:
+        return self.item.find("div", {"class": "card-content-bottom"}).get_text()
+
+    def get_figure(self, *args, **kwargs) -> str:
+        target_domain = kwargs.get("target_domain", None)
+        if target_domain is None:
+            raise ValueError("請提供 TARGET_DOMAIN")
+        return "{}{}".format(target_domain, self.item.select_one("figure.card-image img")["data-src"])
+
+    def get_source_url(self, *args, **kwargs) -> str:
+        target_domain = kwargs.get("target_domain", None)
+        if target_domain is None:
+            raise ValueError("請提供 TARGET_DOMAIN")
+
+        used_this_to_clean = kwargs.get("used_this_to_clean", None)
+        if used_this_to_clean is None:
+            return "{}{}".format(target_domain, self.item.select_one("a.card")["href"])
+        else:
+            return "{}{}".format(target_domain, used_this_to_clean(self.item.select_one("a.card")["href"]))
+
+
+class NpmColParse(ParseInit):
+    def __init__(self, item: bs4.element.Tag):
+        self.item = item
+
+    def get_title(self, *args, **kwargs) -> str:
+        return self.item.find("h3", {"class": "card-title-underline"}).get_text()
+
+    def get_date(self, *args, **kwargs) -> str:
+        return "-"
+
+    def get_address(self, *args, **kwargs) -> str:
+        return self.item.find("div", {"class": "card-content-bottom"}).get_text()
+
+    def get_figure(self, *args, **kwargs) -> str:
+        target_domain = kwargs.get("target_domain", None)
+        if target_domain is None:
+            raise ValueError("請提供 TARGET_DOMAIN")
+        return "{}{}".format(target_domain, self.item.select_one("figure.card-image img")["data-src"])
+
+    def get_source_url(self, *args, **kwargs) -> str:
+        target_domain = kwargs.get("target_domain", None)
+        if target_domain is None:
+            raise ValueError("請提供 TARGET_DOMAIN")
+
+        used_this_to_clean = kwargs.get("used_this_to_clean", None)
+        if used_this_to_clean is None:
+            return "{}{}".format(target_domain, self.item.select_one("a.card")["href"])
+        else:
+            return "{}{}".format(target_domain, used_this_to_clean(self.item.select_one("a.card")["href"]))
+
+
+class CKSMHParse(ParseInit):
+    def __init__(self, item: bs4.element.Tag):
+        self.item = item
+
+    def get_title(self, *args, **kwargs) -> str:
+        return self.item.select_one("dt > a > div.h3").get_text()
+
+    def get_date(self, *args, **kwargs) -> str:
+        return self.item.select_one("dd > span.date").get_text()
+
+    def get_address(self, *args, **kwargs) -> str:
+        return self.item.select_one("dd > span.location").get_text()
+
+    def get_figure(self, *args, **kwargs) -> str:
+        return self.item.select_one("dt > a > span > img")["src"]
+
+    def get_source_url(self, *args, **kwargs) -> str:
+        return self.item.select_one("dt > a")["href"]
