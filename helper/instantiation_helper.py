909abcecabe0433a3d9e8e876455e82b3282c053
justiceqaq <justiceqaq@gmail.com>
2022-05-27 03:57:06 +0800

    add: RequestsJsonInstantiation

--- helper/instantiation_helper.py
+++ helper/instantiation_helper.py
@@ -1,9 +1,10 @@
 from abc import ABCMeta, abstractmethod
+from typing import Dict
 
 from bs4 import BeautifulSoup
 
 from helper.crawler_helper import PyCurlCrawler, RequestsCrawler
-from helper.translation_helper import BeautifulSoupTranslation
+from helper.translation_helper import BeautifulSoupTranslation, JsonTranslation
 
 # 爬蟲 + 轉譯器 組合
 
@@ -19,6 +20,13 @@ class RequestsBeautifulSoupInstantiation(
 ):
     def fetch(self) -> BeautifulSoup:
         context = self.get_page()
+        translation_object = self.format_to_object(context)
+        return translation_object
+
+
+class RequestsJsonInstantiation(InstantiationInit, RequestsCrawler, JsonTranslation):
+    def fetch(self, *args, **kwargs) -> Dict:
+        context = self.get_page(*args, **kwargs)
         translation_object = self.format_to_object(context)
         return translation_object
 
