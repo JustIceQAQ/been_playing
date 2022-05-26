0c5585aa6bc83fbdc104dc7b9b81bd5fd95e3279
justiceqaq <justiceqaq@gmail.com>
2022-05-27 00:35:45 +0800

    add: Exhibition Enum

---
+++ exhibition/__init__.py
@@ -0,0 +1,8 @@
+import enum
+
+
+class ExhibitionEnum(str, enum.Enum):
+    huashan1914 = "huashan1914[華山1914]"
+    cksmh = "cksmh[中正紀念堂]"
+    mocataipei = "mocataipei[台北當代藝術館]"
+    npm = "npm[故宮]"
