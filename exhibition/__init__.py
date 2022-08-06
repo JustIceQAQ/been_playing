a68e06f4d5bd55c69bbe29cfdc576ab251bccf7d
justiceqaq <justiceqaq@gmail.com>
2022-08-07 00:12:46 +0800

    add: ExhibitionInformation dataclass and ExhibitionEnum

--- exhibition/__init__.py
+++ exhibition/__init__.py
@@ -1,15 +1,55 @@
+import dataclasses
 import enum
 
 
-class ExhibitionEnum(str, enum.Enum):
-    huashan1914 = "huashan1914[華山1914]"
-    cksmh = "cksmh[中正紀念堂]"
-    mocataipei = "mocataipei[台北當代藝術館]"
-    npm = "npm[故宮]"
-    songshanculturalpark = "songshanculturalpark[松山文創園區]"
-    ntsec = "ntsec[國立台灣科學教育館]"
-    tfam = "tfam[臺北市立美術館]"
-    tickets_udnfunlife = "tickets_udnfunlife[udn售票網]"
-    tickets_books = "tickets_books[博客來售票網]"
-    ntm = "ntm[國立台灣博物館]"
-    tmc = "tmc[台北流行音樂中心]"
+@dataclasses.dataclass
+class ExhibitionInformation:
+    fullname: str = None
+    code_name: str = None
+
+
+class ExhibitionEnum(ExhibitionInformation, enum.Enum):
+    huashan1914 = (
+        "華山1914文化創意產業園區",
+        "huashan1914",
+    )
+    cksmh = (
+        "中正紀念堂",
+        "cksmh",
+    )
+    mocataipei = (
+        "台北當代藝術館",
+        "mocataipei",
+    )
+    npm = (
+        "國立故宮博物院",
+        "npm",
+    )
+    songshanculturalpark = (
+        "松山文創園區",
+        "songshanculturalpark",
+    )
+    ntsec = (
+        "國立臺灣科學教育館",
+        "ntsec",
+    )
+    tfam = (
+        "臺北市立美術館",
+        "tfam",
+    )
+    tickets_udnfunlife = (
+        "udn售票網",
+        "tickets_udnfunlife",
+    )
+    tickets_books = (
+        "博客來售票網",
+        "tickets_books",
+    )
+    ntm = (
+        "國立臺灣博物館",
+        "ntm",
+    )
+    tmc = (
+        "台北流行音樂中心",
+        "tmc",
+    )
