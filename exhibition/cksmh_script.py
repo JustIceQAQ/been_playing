34550124662ba1ad28e4b7ca7f3c7c5bed673466
justiceqaq <justiceqaq@gmail.com>
2022-09-19 01:48:21 +0800

    fix: cksmh next page fetch

--- exhibition/cksmh_script.py
+++ exhibition/cksmh_script.py
@@ -9,18 +9,28 @@ from helper.storage_helper import Exhibition, JustJsonStorage
 
 def cksmh_script() -> None:
     root_dir = Path(__file__).resolve(strict=True).parent.parent
-    target_url = "https://www.cksmh.gov.tw/activitysoonlist_369.html"
+    target_url = "https://www.cksmh.gov.tw/activitysoonlist_369_{}.html"
     target_storage = str(root_dir / "data" / "cksmh_exhibition.json")
     target_systematics = ExhibitionEnum.cksmh
 
-    requests_worker = RequestsBeautifulSoupInstantiation(target_url)
-    target_object = requests_worker.fetch()
     storage = JustJsonStorage(target_storage, target_systematics)
     storage.truncate_table()
 
-    dataset = target_object.select("ul.exhibition-list li dl")
+    dataset_list = []
 
-    for item in dataset:
+    requests_worker = RequestsBeautifulSoupInstantiation(target_url.format("1"))
+    target_object = requests_worker.fetch()
+    dataset_list.extend(target_object.select("ul.exhibition-list li dl"))
+
+    for n in range(2, 5):
+        requests_worker = RequestsBeautifulSoupInstantiation(target_url.format(n))
+        target_object = requests_worker.fetch()
+        if dataset := target_object.select("ul.exhibition-list li dl"):
+            dataset_list.extend(dataset)
+        else:
+            break
+
+    for item in dataset_list:
         cksmh_data = CKSMHParse(item).parsed()
         cksmh_clean_data = {
             key: RequestsClean.clean_string(value) for key, value in cksmh_data.items()
