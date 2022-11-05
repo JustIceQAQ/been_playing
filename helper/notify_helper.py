9f590856f1446d9207ddfe63c861ac7a6cd5fffe
justiceqaq <justiceqaq@gmail.com>
2022-11-06 00:31:25 +0800

    add: LogNotify

--- helper/notify_helper.py
+++ helper/notify_helper.py
@@ -1,6 +1,8 @@
 import abc
+import logging
 import os
 import pathlib
+from enum import Enum
 
 import requests
 from dotenv import load_dotenv
@@ -16,8 +18,26 @@ class NoneNotify(NotifyInit):
     def __init__(self):
         pass
 
-    def send_message(self, message):
+    def send_message(self, message, *args, **kwargs):
         pass
+
+
+class LogNotify(NotifyInit):
+    class LogLevel(int, Enum):
+        CRITICAL = logging.CRITICAL
+        FATAL = logging.CRITICAL
+        ERROR = logging.ERROR
+        WARNING = logging.WARNING
+        WARN = logging.WARN
+        INFO = logging.INFO
+        DEBUG = logging.DEBUG
+        NOTSET = logging.NOTSET
+
+    def __init__(self, runtime_log: logging.Logger):
+        self.runtime_log: logging.Logger = runtime_log
+
+    def send_message(self, message, level=logging.DEBUG, *args, **kwargs):
+        self.runtime_log.log(level, message)
 
 
 class LineNotify(NotifyInit):
@@ -32,7 +52,7 @@ class LineNotify(NotifyInit):
         if self.token is None:
             raise ValueError("token is empty.")
 
-    def send_message(self, message):
+    def send_message(self, message, *args, **kwargs):
         data = {"message": f"{self.pre_text}\n{message}"}
         requests.post(self.service_path, headers=self.headers, data=data)
 
@@ -41,6 +61,17 @@ if __name__ == "__main__":
     this_env = pathlib.Path(__file__).parent.parent / ".env"
     if this_env.exists():
         load_dotenv(this_env)
-    LINE_NOTIFY_API = os.getenv("LINE_NOTIFY_API", None)
-    line_notify = LineNotify(LINE_NOTIFY_API)
-    line_notify.send_message("0.0")
+    IS_DEBUG = os.getenv("IS_DEBUG", False)
+
+    logging.basicConfig(
+        level=logging.DEBUG if IS_DEBUG else logging.WARNING,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M",
+    )
+
+    runtime_logging = logging.getLogger("runtime_logging")
+    runtime_notify = LogNotify(runtime_logging)
+
+    # LINE_NOTIFY_API = os.getenv("LINE_NOTIFY_API", None)
+    # line_notify = LineNotify(LINE_NOTIFY_API)
+    runtime_notify.send_message("0.0")
