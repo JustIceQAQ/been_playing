1656b43d1797b2c54429507a0260a65d698ec0cd
justiceqaq <justiceqaq@gmail.com>
2022-11-06 00:31:58 +0800

    tweak: notify usage

--- run_all_script.py
+++ run_all_script.py
@@ -23,18 +23,49 @@ from exhibition.tickets_udnfunlife_script import TicketsUdnFunLifeRunner
 from exhibition.tmc_script import TMCRunner
 from exhibition.twtc_script import TWTCRunner
 from helper.image_helper import ImgurImage
-from helper.notify_helper import LineNotify, NoneNotify
+from helper.notify_helper import LineNotify, LogNotify, NoneNotify
 from helper.thread_helper import RuntimeThread
 
 
 def main():
+    # load local env file
+    ROOT_DIR = Path(__file__).resolve(strict=True).parent
+    this_env = ROOT_DIR / ".env"
+    if this_env.exists():
+        load_dotenv(this_env)
+
+    # check env values
+    IS_DEBUG = os.getenv("IS_DEBUG", False)
+    CLIENT_ID = os.getenv("IMGUR_API_CLIENT_ID", False)
+    CLIENT_SECRET = os.getenv("IMGUR_API_CLIENT_SECRET", False)
+    USE_PICKLED = os.getenv("USE_PICKLED", True)
+    LINE_NOTIFY_API = os.getenv("LINE_NOTIFY_API", False)
+
+    # logging init
     logging.basicConfig(
-        level=logging.WARNING,
+        level=logging.DEBUG if IS_DEBUG else logging.WARNING,
         format="%(asctime)s %(levelname)s %(message)s",
         datefmt="%Y-%m-%d %H:%M",
     )
-
     runtime_logging = logging.getLogger("runtime_logging")
+
+    # Imgur image
+    file_path = ROOT_DIR / "data" / "cache_file.json"
+    imgur_image = ImgurImage()
+    imgur_image.load_cache_file(file_path)
+
+    # local notify (use logging)
+    log_notify = LogNotify(runtime_logging)
+
+    # if runtime process except
+    if IS_DEBUG is False and LINE_NOTIFY_API:
+        runtime_notify = LineNotify(LINE_NOTIFY_API)
+    else:
+        runtime_notify = NoneNotify()
+
+    if CLIENT_ID and CLIENT_SECRET:
+        imgur_image.login(CLIENT_ID, CLIENT_SECRET)
+        log_notify.send_message("imgur api is login", log_notify.LogLevel.INFO)
 
     py_class_script = {
         CKSMHRunner,
@@ -56,29 +87,9 @@ def main():
         TicketsBooksRunner,
         TicketsUdnFunLifeRunner,
     }
-    ROOT_DIR = Path(__file__).resolve(strict=True).parent
-    runtime_logging.debug(ROOT_DIR)
 
-    file_path = ROOT_DIR / "data" / "cache_file.json"
-    runtime_logging.debug(file_path)
-    imgur_image = ImgurImage()
-    imgur_image.load_cache_file(file_path)
-
-    this_env = ROOT_DIR / ".env"
-    if this_env.exists():
-        runtime_logging.debug(this_env)
-        load_dotenv(this_env)
-    CLIENT_ID = os.getenv("IMGUR_API_CLIENT_ID", False)
-    CLIENT_SECRET = os.getenv("IMGUR_API_CLIENT_SECRET", False)
-    USE_PICKLED = os.getenv("USE_PICKLED", True)
-    LINE_NOTIFY_API = os.getenv("LINE_NOTIFY_API", False)
-
-    if CLIENT_ID and CLIENT_SECRET:
-        imgur_image.login(CLIENT_ID, CLIENT_SECRET)
-        runtime_logging.debug("imgur api is logined")
-    runtime_notify = NoneNotify()
-    if LINE_NOTIFY_API:
-        runtime_notify = LineNotify(LINE_NOTIFY_API)
+    log_notify.send_message(ROOT_DIR, log_notify.LogLevel.INFO)
+    log_notify.send_message(file_path, log_notify.LogLevel.INFO)
 
     all_script_runners = []
 
@@ -96,12 +107,16 @@ def main():
 
     for runner in all_script_runners:
         runner.start()
-    runtime_logging.debug("All Script Runners Threading Is Start")
+    log_notify.send_message(
+        "All Script Runners Threading Is Start", log_notify.LogLevel.INFO
+    )
 
     for runner in all_script_runners:
         runner.join()
     imgur_image.save_cache_file()
-    runtime_logging.debug("All Script Runners Threading Is Done")
+    log_notify.send_message(
+        "All Script Runners Threading Is Done", log_notify.LogLevel.INFO
+    )
 
 
 if __name__ == "__main__":
