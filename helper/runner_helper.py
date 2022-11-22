642ac3ac54ddc43627013fd0aad98fa94baeef36
justiceqaq <justiceqaq@gmail.com>
2022-11-22 15:24:17 +0800

    refactor: RunerAsyncInit

--- helper/runner_helper.py
+++ helper/runner_helper.py
@@ -7,6 +7,74 @@ from exhibition import ExhibitionEnum
 from helper.storage_helper import Exhibition, JustJsonStorage
 
 
+class RunerAsyncInit(metaclass=ABCMeta):
+    storage = None
+    target_storage = None
+    target_systematics = None
+    use_storage = JustJsonStorage
+    exhibition_model = Exhibition
+
+    def __init__(self, need_init=True):
+        super().__init__()
+        self.need_init = need_init
+
+    async def init_storage(self):
+        if hasattr(self, "use_storage") and self.need_init:
+            self.storage = self.use_storage(
+                self.target_storage, self.target_systematics
+            )
+            self.storage.truncate_table()
+
+    async def write_storage(self, data, use_pickled):
+        if self.storage is not None:
+            self.storage.create_data(data, pickled=use_pickled)
+
+    async def commit_storage(self):
+        if self.storage is not None:
+            self.storage.commit()
+
+    @abstractmethod
+    async def get_response(self, *args, **kwargs) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_items(self, *args, **kwargs) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_parsed(self, *args, **kwargs):
+        raise NotImplementedError
+
+    async def get_visit(self, *args, **kwargs):
+        pass
+
+    async def get_cookie(self, *args, **kwargs):
+        pass
+
+    async def run(self, use_pickled=True):
+        await self.init_storage()
+        try:
+            response = await self.get_response()
+            items = await self.get_items(response)
+            exhibitions = self.get_parsed(items)
+
+            for exhibition in exhibitions:
+                await self.write_storage(exhibition.dict(), use_pickled)
+
+            if self.storage is not None:
+                if opening := await self.get_visit():
+                    self.storage.set_visit({"opening": opening})
+        except (ConnectionError, NewConnectionError, ConnectTimeoutError, ProxyError):
+            await self.write_storage(
+                Exhibition(systematics=ExhibitionEnum.BUG, source_url="BUG").dict(),
+                use_pickled,
+            )
+        except Exception as e:
+            raise e
+        finally:
+            await self.commit_storage()
+
+
 class RunnerInit(metaclass=ABCMeta):
     storage = None
     target_storage = None
