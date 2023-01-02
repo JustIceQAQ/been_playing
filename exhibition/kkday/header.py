f7e1a0a9af29c72a1fc91b58bd5a2a5a7743be56
justiceqaq <justiceqaq@gmail.com>
2023-01-02 17:45:02 +0800

    add: KKDay exhibition

--- exhibition/kkday/header.py
+++ exhibition/kkday/header.py
@@ -10,4 +10,7 @@ class KKDayHeader(HeaderInit):
             "User-Agent": random.choice(USER_AGENT_LIST),
             "host": "www.kkday.com",
             "Referer": "https://www.kkday.com/zh-tw/country/taiwan/events-and-exhibitions?cat=TAG_3&sort=prec&page=1",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7",
         }
