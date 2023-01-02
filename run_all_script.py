f98d046947e20a5f03b676ca357d1acf0b6ce147
justiceqaq <justiceqaq@gmail.com>
2023-01-02 17:50:55 +0800

    refactor: all_script_runners

--- run_all_script.py
+++ run_all_script.py
@@ -5,28 +5,7 @@ from pathlib import Path
 import sentry_sdk
 from dotenv import load_dotenv
 
-from exhibition.cksmh.script import CKSMHRunner
-from exhibition.huashan1914.script import HuaShan1914Runner
-from exhibition.ibon.script import IBonRunner
-from exhibition.jam.script import JamRunner
-from exhibition.kkday.script import KKDayRunner
-from exhibition.kktix.script import KKTixRunner
-from exhibition.klook.script import KLookRunner
-from exhibition.mocataipei.script import MocaTaipeiRunner
-from exhibition.museum_post.script import MuseumPostRunner
-from exhibition.mwr.script import MWRRunner
-from exhibition.ncpi.script import NCPIRunner
-from exhibition.nmh.script import NMHRunner
-from exhibition.npm.script import NPMRunner
-from exhibition.ntm.script import NTMRunner
-from exhibition.ntsec.script import NTSECRunner
-from exhibition.opentix.script import OpenTixRunner
-from exhibition.songshanculturalpark.script import SongShanCulturalParkRunner
-from exhibition.tfam.script import TFAMRunner
-from exhibition.tickets_books.script import TicketsBooksRunner
-from exhibition.tickets_udnfunlife.script import TicketsUdnFunLifeRunner
-from exhibition.tmc.script import TMCRunner
-from exhibition.twtc.script import TWTCRunner
+from exhibition.script import PY_CLASS_SCRIPT
 from helper.image_helper import ImgurImage
 from helper.notify_helper import LineNotify, LogNotify, NoneNotify
 from helper.thread_helper import RuntimeThread
@@ -66,31 +45,6 @@ def main():
         imgur_image.login(CLIENT_ID, CLIENT_SECRET)
         log_notify.send_message("imgur api is login", log_notify.LogLevel.INFO)
 
-    py_class_script = {
-        CKSMHRunner,
-        HuaShan1914Runner,
-        JamRunner,
-        MWRRunner,
-        MocaTaipeiRunner,
-        MuseumPostRunner,
-        NCPIRunner,
-        NMHRunner,
-        NPMRunner,
-        NTMRunner,
-        NTSECRunner,
-        OpenTixRunner,
-        SongShanCulturalParkRunner,
-        TFAMRunner,
-        TMCRunner,
-        TWTCRunner,
-        TicketsBooksRunner,
-        TicketsUdnFunLifeRunner,
-        KLookRunner,
-        KKTixRunner,
-        IBonRunner,
-        KKDayRunner,
-    }
-
     log_notify.send_message(ROOT_DIR, log_notify.LogLevel.INFO)
     log_notify.send_message(file_path, log_notify.LogLevel.INFO)
 
@@ -104,7 +58,7 @@ def main():
                 name=py_class.__name__,
                 kwargs={"use_pickled": USE_PICKLED},
             )
-            for py_class in py_class_script
+            for py_class in PY_CLASS_SCRIPT
         ]
     )
 
@@ -130,8 +84,9 @@ if __name__ == "__main__":
         load_dotenv(this_env)
 
     # set sentry
-    IS_DEBUG = os.getenv("IS_DEBUG", False)
-    SENTRY_SDK_DNS = os.getenv("SENTRY_SDK_DNS", None) if not IS_DEBUG else None
+    SENTRY_SDK_DNS = (
+        os.getenv("SENTRY_SDK_DNS", None) if not os.getenv("IS_DEBUG", False) else None
+    )
 
     sentry_sdk.init(dsn=SENTRY_SDK_DNS, traces_sample_rate=1.0)
 
