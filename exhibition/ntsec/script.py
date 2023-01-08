ebae845ba68946944f9e0791ed6f1421c793e7fe
justiceqaq <justiceqaq@gmail.com>
2023-01-08 20:04:28 +0800

    add: NTSEC visit

--- exhibition/ntsec/script.py
+++ exhibition/ntsec/script.py
@@ -15,6 +15,7 @@ class NTSECRunner(RunnerInit):
     target_url = "https://www.ntsec.gov.tw/article/list.aspx?a=25"
     use_method = "GET"
     target_domain = "https://www.ntsec.gov.tw"
+    target_visit_url = "https://www.ntsec.gov.tw/article/detail.aspx?a=80"
     target_storage = str(root_dir / "data" / "ntsec_exhibition.json")
     target_systematics = ExhibitionEnum.NTSEC
     instantiation = RequestsBeautifulSoupInstantiation
@@ -44,6 +45,38 @@ class NTSECRunner(RunnerInit):
             # print(exhibition)
             yield exhibition
 
+    def get_visit(self, *args, **kwargs):
+        requests_worker = self.instantiation(self.target_visit_url)
+        headers = (
+            self.use_header().get_header() if self.use_header is not None else None
+        )
+        response = requests_worker.fetch(self.use_method, headers=headers)
+        newsin_text = response.find("div", {"class": "newsin-text"})
+        title_obj = newsin_text.find("h2")
+        title = title_obj.get_text().strip()
+        newsin_text_table = newsin_text.find("table", {"class": "newsin-text-table"})
+        openings = "\n".join(
+            [
+                " | ".join([td.get_text().strip() for td in tr.find_all("td")])
+                for tr in newsin_text_table.select("tbody > tr")
+            ]
+        )
+        suffix_info = (
+            has_suffix_info.get_text().strip()
+            if (has_suffix_info := newsin_text_table.next_sibling.next_sibling)
+            else ""
+        )
+
+        openings_list = [
+            title,
+            title_obj.next_sibling.get_text().strip(),
+            openings,
+            suffix_info,
+        ]
+
+        return "\n".join(openings_list)
+
 
 if __name__ == "__main__":
     NTSECRunner().run(use_pickled=False)
+    # NTSECRunner().get_visit()
