36d6acee734b0096348006580d57722c0581d224
justiceqaq <justiceqaq@gmail.com>
2023-03-11 19:38:58 +0800

    fix: NPMRunner

--- helper/crawler/scrape_do.py
+++ helper/crawler/scrape_do.py
@@ -16,8 +16,8 @@ class ScrapeDoCrawler(CrawlerInit):
         self.api_url = api_url
 
     def get_page(self, url, formatted="text"):
-        full_url = urlencode({"token": self.token, "url": url})
-        response = requests.get(f"{self.api_url}?{full_url}")
+        query_parameters = urlencode({"token": self.token, "url": url, "render": True})
+        response = requests.get(f"{self.api_url}?{query_parameters}")
 
         if formatted in {self.Formatted.text}:
             return response.text
@@ -26,6 +26,8 @@ class ScrapeDoCrawler(CrawlerInit):
 
 
 if __name__ == "__main__":
-    dd = ScrapeDoCrawler(token="d0ba7028c1604b21b8bdae1076703b1d5d60408511f")
-    ok = dd.get_page(url="https://www.klook.com/zh-TW/event/19-taipei/")
+    dd = ScrapeDoCrawler(token="")
+    ok = dd.get_page(
+        url="https://www.npm.gov.tw/Exhibition-Current.aspx?sno=03000060&l=1&type=1"
+    )
     print(ok)
