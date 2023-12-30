a87eed2d0d8bad74c9f1265cdea1e32328646873
justiceqaq <justiceqaq@gmail.com>
2023-12-30 22:44:16 +0800

    fix: NMHRunner new website format

--- exhibition/nmh/parse.py
+++ exhibition/nmh/parse.py
@@ -8,20 +8,24 @@ class NMHParse(ParseInit):
         self.item = item
 
     def get_title(self, *args, **kwargs) -> str:
-        return self.item.find("p", {"class": "tit"}).get_text()
+        return self.item.find("div", {"class": "caption"}).get_text().strip()
 
     def get_date(self, *args, **kwargs) -> str:
-        return self.item.find("p", {"class": "time"}).get_text()
+        date_str = self.item.find("p", {"class": "activity-time"}).get_text()
+        if date_str:
+            date_str = date_str.replace("時間：", "").strip()
+        return date_str
 
     def get_address(self, *args, **kwargs) -> str:
         runtime_address = None
-        if address_element := self.item.find("p", {"class": "address"}):
-            runtime_address = address_element.get_text()
-
+        if address_element := self.item.find("p", {"class": "activity-season"}):
+            if runtime_address := address_element.get_text():
+                runtime_address = runtime_address.replace("場地：", "").strip()
         return runtime_address
 
     def get_figure(self, *args, **kwargs) -> str:
         return self.item.find("img").get("src", None)
 
     def get_source_url(self, *args, **kwargs) -> str:
-        return self.item.find("a").get("href", None)
+        return self.item.find("a",
+                              {"class": "div", "target": "_blank"}).get("href", None)
