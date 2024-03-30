f2e7600864885d56a2a916b3a20bd2f7dd829d82
justiceqaq <justiceqaq@gmail.com>
2024-03-30 23:43:04 +0800

    tweak: TWTCParse get_date format

--- exhibition/twtc/parse.py
+++ exhibition/twtc/parse.py
@@ -1,3 +1,5 @@
+import datetime
+
 import bs4
 
 from helper.parse_helper import ParseInit
@@ -12,7 +14,15 @@ class TWTCParse(ParseInit):
         return self.item_tds[1].get_text()
 
     def get_date(self, *args, **kwargs) -> str:
-        return self.item_tds[0].get_text()
+        this_year = datetime.datetime.now().year
+        raw_date_string = self.item_tds[0].get_text()
+        start_date_str, end_date_str = raw_date_string.split("~")
+        start_date_month, start_date_day = start_date_str.split("/")
+        end_date_month, end_date_day = end_date_str.split("/")
+        string_date = f"{this_year}-{start_date_month}-{start_date_day}"
+        end_date_year = (this_year + 1) if int(end_date_month) < int(start_date_month) else this_year
+        end_date = f"{end_date_year}-{end_date_month}-{end_date_day}"
+        return f"{string_date} ~ {end_date}"
 
     def get_address(self, *args, **kwargs) -> str:
         return self.item_tds[4].get_text()
