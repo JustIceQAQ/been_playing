4c50c42cc1b153588338794963d1040e6ecef033
justiceqaq <justiceqaq@gmail.com>
2024-10-23 00:22:36 +0800

    tweak: replace_string_set

--- helper/clean_helper.py
+++ helper/clean_helper.py
@@ -13,7 +13,7 @@ class RequestsClean(CleanInit):
         # TODO: big5 utf-8
         if raw_string is None:
             return None
-        replace_string_set = {"日期：", " more"}
+        replace_string_set = {"日期：", " more", "地點："}
         for replace_string in replace_string_set:
             raw_string = raw_string.replace(replace_string, " ")
         return " ".join(raw_string.split()).strip()
