8f968d4311de92e468cd93a226320870930b9752
justiceqaq <justiceqaq@gmail.com>
2024-10-23 01:16:48 +0800

    add: settings config

---
+++ configs/settings.py
@@ -0,0 +1,35 @@
+import os
+from functools import lru_cache
+
+from pydantic import Field
+from pydantic.config import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    IS_DEBUG: bool = Field(description="DEBUG 模式", default=False)
+
+    IMGUR_API_CLIENT_ID: str
+    IMGUR_API_CLIENT_SECRET: str
+
+    SCRAPER_API_KEY: str
+    SCRAPE_DO_API_KEY: str
+
+    LINE_NOTIFY_API: str
+
+    SENTRY_SDK_DNS: str
+
+
+class LocalSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+class ActionSettings(BaseSettings):
+    pass
+
+
+@lru_cache
+def get_settings() -> Settings:
+    is_debug = os.getenv("IS_DEBUG")
+    if is_debug:
+        return LocalSettings()
+    return ActionSettings()
