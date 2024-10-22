398c9b4ae1e68c23c80e78ad3852a514324cb349
justiceqaq <justiceqaq@gmail.com>
2024-10-23 00:23:31 +0800

    add: datetime_now and datetime_now_iso_format method

--- helper/utils_helper.py
+++ helper/utils_helper.py
@@ -4,5 +4,13 @@ import zoneinfo
 TAIWAN_TIMEZONE = zoneinfo.ZoneInfo("Asia/Taipei")
 
 
+def datetime_now() -> dt.datetime:
+    return dt.datetime.now(tz=TAIWAN_TIMEZONE)
+
+
 def date_now() -> dt.date:
-    return dt.datetime.now(tz=TAIWAN_TIMEZONE).date()
+    return datetime_now().date()
+
+
+def datetime_now_iso_format() -> str:
+    return datetime_now().isoformat()
