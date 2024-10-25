fe8c1b4342c8e5c56c4d0bcb04375e1d1e3c7f95
justiceqaq <justiceqaq@gmail.com>
2024-10-25 22:08:32 +0800

    add: KingCarArt

--- app/script.py
+++ app/script.py
@@ -2,6 +2,8 @@ from .exhibition.cksmh.script import CKSMHRunner
 from .exhibition.clab.script import CLabRunner
 from .exhibition.fubonartmuseum.script import FuBonArtMuseumRunner
 from .exhibition.huashan1914.script import HuaShan1914Runner
+from .exhibition.jam.script import JamRunner
+from .exhibition.kingcarart.script import KingCarArtRunner
 from .platform.ibon.script import IBonRunner
 
 PY_CLASS_SCRIPT = {
@@ -9,5 +11,7 @@ PY_CLASS_SCRIPT = {
     CLabRunner,
     FuBonArtMuseumRunner,
     HuaShan1914Runner,
+    JamRunner,
     IBonRunner,
+    KingCarArtRunner,
 }
