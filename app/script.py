d9b961506561e6ee9d699624b68c1f5f35c8b3e6
justiceqaq <justiceqaq@gmail.com>
2024-10-26 21:18:07 +0800

    add: MuseumPostRunner

--- app/script.py
+++ app/script.py
@@ -5,6 +5,7 @@ from .exhibition.huashan1914.script import HuaShan1914Runner
 from .exhibition.jam.script import JamRunner
 from .exhibition.kingcarart.script import KingCarArtRunner
 from .exhibition.mocataipei.script import MoCaTaipeiRunner
+from .exhibition.museumpost.script import MuseumPostRunner
 from .platform.ibon.script import IBonRunner
 from .platform.kkday.script import KKDayRunner
 from .platform.kktix.script import KKTixRunner
@@ -22,4 +23,5 @@ PY_CLASS_SCRIPT = {
     KKTixRunner,
     KLookRunner,
     MoCaTaipeiRunner,
+    MuseumPostRunner,
 }
