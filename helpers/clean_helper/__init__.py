20f0c508dc721a7cc124507c7394812c312c822f
JustIceQAQ <justiceqaq@gmail.com>
2025-04-21 01:17:43 +0800

    add: clean_helper

---
+++ helpers/clean_helper/__init__.py
@@ -0,0 +1,10 @@
+class RequestsClean:
+    @staticmethod
+    def clean_string(raw_string: str) -> str:
+        # TODO: big5 utf-8
+        if raw_string is None:
+            return None
+        replace_string_set = {"日期：", " more", "地點："}
+        for replace_string in replace_string_set:
+            raw_string = raw_string.replace(replace_string, " ")
+        return " ".join(raw_string.split()).strip()
