dc4910ff53ec11d400889f08adff3b357a51f7c8
JustIceQAQ <justiceqaq@gmail.com>
2025-04-24 09:01:42 +0800

    add: load_file_to_object

--- helpers/translation/beautiful_soup/helper.py
+++ helpers/translation/beautiful_soup/helper.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from bs4 import BeautifulSoup
 
 from helpers.translation.base import TranslationInit
@@ -8,3 +10,8 @@ class BeautifulSoupTranslation(TranslationInit):
         self, text: str, format_encoding: str | None = "html5lib", *args, **kwargs
     ) -> BeautifulSoup:
         return BeautifulSoup(text, format_encoding)
+
+    def load_file_to_object(
+        self, file: pathlib.Path, format_encoding: str | None = "html5lib"
+    ) -> BeautifulSoup:
+        return BeautifulSoup(file.read_text(encoding="utf-8"), format_encoding)
