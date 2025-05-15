f67268633ec15ee2f71dba74392f3fe188cb6740
JustIceQAQ <justiceqaq@gmail.com>
2025-05-15 10:46:40 +0800

    add: google_map_place_id

--- app/exhibition/mocataipei/script.py
+++ app/exhibition/mocataipei/script.py
@@ -25,13 +25,8 @@ class MoCaTaipeiRunner(RunnerInit):
             fullname="台北當代藝術館",
             code_name="MoCaTaipei",
             external_link="https://www.mocataipei.org.tw/tw/ExhibitionAndEvent",
-            map_url=(
-                "https://www.google.com/maps/embed?"
-                "pb=!1m18!1m12!1m3!1d3614.5072784351205!2d121.51563993159156!"
-                "3d25.05079012059311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!"
-                "3m3!1m2!1s0x3442a96c49f1e473%3A0xb4f74ae9ed21c1fb!2z5Y-"
-                "w5YyX55W25Luj6Jed6KGT6aSo!5e0!3m2!1szh-TW!2stw!4v1739201393858!5m2!1szh-TW!2stw"
-            ),
+            address="台北市大同區103長安西路39號 ",
+            google_map_place_id="ChIJc-TxSWypQjQR-8Eh7elK97Q",
         )
 
     async def fetch_response(self):
