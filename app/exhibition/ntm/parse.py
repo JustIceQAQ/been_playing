e66a0ae8589d4f02d8f0787bcfa69fe704dc2d53
JustIceQAQ <justiceqaq@gmail.com>
2025-07-01 16:24:58 +0800

    tweak: NtmRunner all_branch

--- app/exhibition/ntm/parse.py
+++ app/exhibition/ntm/parse.py
@@ -1,14 +1,45 @@
-import dataclasses
-
 import bs4
 
 from helpers.parse_helper import ParseInit
 
+from pydantic import BaseModel, Field
 
-@dataclasses.dataclass
-class PathQuery:
+
+class PathQuery(BaseModel):
     n: int
     sms: int
+    csn: int = Field(alias="_CSN")
+
+
+csn_list = [2446, 4479, 4477]
+
+main_build: list[PathQuery] = [
+    PathQuery(n=5472, sms=13389, _CSN=csn) for csn in csn_list
+]
+
+natural_history_branch: list[PathQuery] = [
+    PathQuery(n=5473, sms=13389, _CSN=csn) for csn in csn_list
+]
+
+nanmen_branch: list[PathQuery] = [
+    PathQuery(n=5474, sms=13389, _CSN=csn) for csn in csn_list
+]
+
+railway_department_park: list[PathQuery] = [
+    PathQuery(n=5478, sms=13389, _CSN=csn) for csn in csn_list
+]
+
+other_venue: list[PathQuery] = [
+    PathQuery(n=5477, sms=13389, _CSN=csn) for csn in csn_list
+]
+
+all_branch: list[list[PathQuery]] = [
+    main_build,
+    natural_history_branch,
+    nanmen_branch,
+    railway_department_park,
+    other_venue,
+]
 
 
 class NtmParse(ParseInit):
