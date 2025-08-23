15c14ab5dd171d9e29a402fb3c16e37e78169266
JustIceQAQ <justiceqaq@gmail.com>
2025-08-23 21:06:58 +0800

    fix: clean_string if isinstance list case

--- helpers/clean_helper/__init__.py
+++ helpers/clean_helper/__init__.py
@@ -1,9 +1,16 @@
+from typing import Any
+
+Values = str | list[Any]
+
+
 class RequestsClean:
     @staticmethod
-    def clean_string(raw_string: str) -> str:
+    def clean_string(raw_string: Values) -> Values:
         # TODO: big5 utf-8
         if raw_string is None:
             return None
+        if isinstance(raw_string, list):
+            return raw_string
         replace_string_set = {"日期：", " more", "地點："}
         for replace_string in replace_string_set:
             raw_string = raw_string.replace(replace_string, " ")
