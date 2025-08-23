6ea23f3e809f84691e28d90dc0601aee15d95b78
JustIceQAQ <justiceqaq@gmail.com>
2025-08-23 21:06:23 +0800

    add: get_tags

--- app/exhibition/clab/script.py
+++ app/exhibition/clab/script.py
@@ -1,8 +1,12 @@
+import asyncio
+
 import bs4
 
 from app.exhibition.clab.parse import CLabParse
+from helpers.cache import NoneCache
 from helpers.crawler.httpx.helper import HttpxAsyncClient
 from helpers.headers_helper import get_header
+from helpers.image.none.helper import NoneImage
 from helpers.runner.helper import RunnerInit
 from helpers.storage.helper import Information
 from helpers.translation.beautiful_soup import BeautifulSoupTranslation
@@ -45,3 +49,11 @@ class CLabRunner(RunnerInit):
     async def fetch_parsed(self):
         parsed: bs4.BeautifulSoup = await super().fetch_parsed()
         return parsed.find_all("div", {"data-aos": "-block-line"})
+
+
+async def main():
+    await CLabRunner().run(NoneCache(), NoneImage())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
