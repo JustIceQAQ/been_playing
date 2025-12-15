e824ca0e3947db6464146355095ffcb707b97309
JustIceQAQ <justiceqaq@gmail.com>
2025-12-15 12:53:51 +0800

    fix: NtcArtMuseumRunner

--- app/exhibition/ntc_art_museum/parse.py
+++ app/exhibition/ntc_art_museum/parse.py
@@ -1,97 +1,62 @@
-import bs4
+import datetime
+import re
 
 from helpers.parse_helper import ParseInit
+from helpers.utils_helper import TAIWAN_TIMEZONE
 
 
-class NtcArtMuseumMainParse(ParseInit):
-    def __init__(self, item: bs4.element.Tag):
+class NtcArtMuseumParse(ParseInit):
+    def __init__(self, item: dict):
         self.item = item
 
     def get_title(self, *args, **kwargs) -> str:
-        return self.item.find("img").get("alt")
-
-    def get_date(self, *args, **kwargs) -> str:
-        date_format = ""
-        year = (
-            self.item.select_one("div.text-group > div.upper-text > h1")
-            .get_text()
-            .strip()
-        )
-        dates = (
-            self.item.select_one("div.text-group > div.bottom-text").get_text().strip()
-        )
-        left_date, _, right_date = dates.partition("—")
-        clean_left_date = left_date.strip()
-        clean_right_date = right_date.strip()
-        has_left_month = None
-        if clean_left_date:
-            left_month, _, left_month_day = clean_left_date.partition(".")
-            date_format += f"{year}-{left_month}-{left_month_day}"
-            has_left_month = left_month
-        if clean_right_date:
-            right_month, _, right_month_day = clean_right_date.partition(".")
-            if has_left_month:
-                if int(right_month) < int(has_left_month):
-                    year = int(year) + 1
-            date_format += f" ~ {year}-{right_month}-{right_month_day}"
-        return date_format
-
-    def get_address(self, *args, **kwargs) -> str:
-        return ""
-
-    def get_figure(self, *args, **kwargs) -> str:
-        return self.item.find("img").get("src")
-
-    def get_source_url(self, *args, **kwargs) -> str:
-        target_domain = kwargs.get("target_domain", None)
-        if target_domain is None:
-            raise ValueError("請提供 TARGET_DOMAIN")
-        return "{}{}".format(target_domain, self.item.get("href"))
-
-
-class NtcArtMuseumOtherParse(ParseInit):
-    def __init__(self, item: bs4.element.Tag):
-        self.item = item
-
-    def get_title(self, *args, **kwargs) -> str:
-        return self.item.find("h2", {"class": "bigTitle"}).get_text().strip()
+        return self.item.get("ExName")
 
     def get_date(self, *args, **kwargs) -> str | None:
-        raw_date = self.item.find("p", {"class": "bigDate"})
-        if raw_date is None:
+        pattern = r"\((\d+)\)"
+        ex_date_st: str | None = self.item.get("ExDateSt", None)
+        ex_date_en: str | None = self.item.get("ExDateEn", None)
+
+        if ex_date_st is None:
             return None
-        raw_date_value = raw_date.get_text().strip()
-        if "—" not in raw_date_value:
-            return raw_date_value
 
-        start_date, end_date = raw_date_value.split("—")
-        start_date = start_date.strip()
-        end_date = end_date.strip()
+        ex_date_st_match = re.search(pattern, ex_date_st)
+        ex_date_st_formatted_date = None
+        if ex_date_st_match:
+            ex_date_st_timestamp_str = ex_date_st_match.group(1)
+            ex_date_st_timestamp_ms = int(ex_date_st_timestamp_str) / 1000
+            ex_date_st_object_utc = datetime.datetime.fromtimestamp(
+                ex_date_st_timestamp_ms, TAIWAN_TIMEZONE
+            )
+            ex_date_st_formatted_date = ex_date_st_object_utc.strftime("%Y-%m-%d")
 
-        start_date_year = start_date[:4]
+        if ex_date_en is None:
+            return ex_date_st_formatted_date
 
-        if len(end_date.split("/")) == 2:
-            year = start_date_year
-            left_year, left_month, left_month_day = start_date.split("-")
+        ex_date_en_match = re.search(pattern, ex_date_en)
+        ex_date_en_formatted_date = None
+        if ex_date_en_match:
+            ex_date_en_timestamp_str = ex_date_en_match.group(1)
+            ex_date_en_timestamp_ms = int(ex_date_en_timestamp_str) / 1000
+            ex_date_en_object_utc = datetime.datetime.fromtimestamp(
+                ex_date_en_timestamp_ms, TAIWAN_TIMEZONE
+            )
+            ex_date_en_formatted_date = ex_date_en_object_utc.strftime("%Y-%m-%d")
 
-            right_month, _, right_month_day = end_date.partition("/")
-            if int(right_month) < int(left_month):
-                year = int(start_date_year) + 1
-            end_date = str(year) + "/" + end_date
-            end_date = end_date.replace("/", "-")
-        else:
-            pass
-
-        return start_date + " ~ " + end_date
+        return ex_date_st_formatted_date + " ~ " + ex_date_en_formatted_date
 
     def get_address(self, *args, **kwargs) -> str:
-        return ""
+        return self.item.get("ExLocationTextTW").split("–")[1].strip()
+
+    def get_tags(self, *args, **kwargs) -> list[str] | None:
+        ex_tag_set_text_tw: str | None = self.item.get("ExTagSetTextTW", None)
+        if ex_tag_set_text_tw is None:
+            return None
+        return ex_tag_set_text_tw.split(", ")
 
     def get_figure(self, *args, **kwargs) -> str:
-        return self.item.find("img").get("src")
+        return "https://admin.ntcart.museum/Upload/" + self.item.get("ExListImg")
 
     def get_source_url(self, *args, **kwargs) -> str:
-        target_domain = kwargs.get("target_domain", None)
-        if target_domain is None:
-            raise ValueError("請提供 TARGET_DOMAIN")
-        return "{}{}".format(target_domain, self.item.get("href"))
+        ex_no = self.item.get("ExNo")
+        return "https://ntcart.museum/exhibition_content.aspx?id=" + ex_no
