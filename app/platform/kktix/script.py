344531ce5e0dfe9abeb779343b56ffacab7fda16
JustIceQAQ <justiceqaq@gmail.com>
2025-12-24 15:10:46 +0800

    fix: KKTixRunner

--- app/platform/kktix/script.py
+++ app/platform/kktix/script.py
@@ -66,6 +66,7 @@ class KKTixRunner(RunnerInit):
             proxies = [Proxy.all(
                 runtime_settings.PROXY_POOL
             )]
+            print("Has PROXY_POOL !!!")
         async with RNetAsyncClient(
                 proxies=proxies,
         ) as client:
@@ -84,6 +85,7 @@ class KKTixRunner(RunnerInit):
                         )
                         is None
                 ):
+                    print(this_response_text)
                     break
                 responses.append(this_response_text)
                 page += 1
