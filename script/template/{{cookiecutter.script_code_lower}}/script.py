2f0b543b6bcbd258df95795dbda64d7b7808da4f
JustIceQAQ <justiceqaq@gmail.com>
2025-12-29 14:20:29 +0800

    add: Information location_code

--- script/template/{{cookiecutter.script_code_lower}}/script.py
+++ script/template/{{cookiecutter.script_code_lower}}/script.py
@@ -1,10 +1,12 @@
 import asyncio
 
 import bs4
+
 from app.{{cookiecutter.target_sub_directory}}.{{cookiecutter.script_code_lower}}.parse import {{cookiecutter.script_code}}Parse
 from helpers.headers_helper import get_header
 from helpers.runner.helper import RunnerInit
 from helpers.storage.helper import Information, Coordinate
+from helpers.storage.symbol import TaiwanCity
 from helpers.crawler.httpx.helper import HttpxAsyncClient
 from helpers.translation.beautiful_soup import BeautifulSoupTranslation
 from helpers.utils_helper import month_3
@@ -21,6 +23,7 @@ class {{cookiecutter.script_code}}Runner(RunnerInit):
 
     def set_information(self) -> "Information":
         return Information(
+            location_code=TaiwanCity.taipei_city,
             fullname="",
             code_name="",
             external_link="",
