644e94ee9d287cbf0fa8bade8a2ae7708c8ae74f
JustIceQAQ <justiceqaq@gmail.com>
2026-01-23 00:44:11 +0800

    add: analysis to clusters

--- helpers/analysis/agglomerative/__init__.py
+++ helpers/analysis/agglomerative/__init__.py
@@ -2,8 +2,8 @@ import json
 
 import pandas as pd
 import numpy as np
-from sklearn.cluster import DBSCAN
-from sklearn.metrics.pairwise import haversine_distances
+
+from sklearn.cluster import AgglomerativeClustering
 import aiofiles
 import asyncio
 import folium
@@ -11,7 +11,7 @@ import pathlib
 import branca.element as be
 import matplotlib.colors as mcolors
 
-ROOT_PATH = pathlib.Path(__file__).parent.parent.parent.absolute()
+ROOT_PATH = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
 DATA_PATH = ROOT_PATH / "data" / "v2"
 DOCS_PATH = ROOT_PATH / "docs"
 
@@ -21,7 +21,7 @@ MIN_SAMPLES = 2  # 最小點數
 
 
 async def get_json_files(json_file: pathlib.Path):
-    async with aiofiles.open(json_file, mode='r') as f:
+    async with aiofiles.open(json_file, mode="r") as f:
         json_content = await f.read()
     return json.loads(json_content)
 
@@ -42,7 +42,11 @@ def clean_data(centers: list[dict]) -> list[dict]:
             continue
         if isinstance(branch_coordinates, list):
             for branch_coordinate in branch_coordinates:
-                name = "None" if (this_name := branch_coordinate.get("name")) is None else this_name
+                name = (
+                    "None"
+                    if (this_name := branch_coordinate.get("name")) is None
+                    else this_name
+                )
                 ok_centers.append(
                     {
                         "fullname": fullname + "-" + name,
@@ -56,49 +60,42 @@ def clean_data(centers: list[dict]) -> list[dict]:
     return ok_centers
 
 
-def dbscan(df: pd.DataFrame):
-    coords = df[['latitude', 'longitude']].values
-    print(coords)
-    coords_float = coords.astype(np.float64)
-    coords_rad = np.radians(coords_float)
-    km_dist_matrix = haversine_distances(coords_rad, coords_rad) * EARTH_RADIUS_KM
+def hierarchical_clustering(df: pd.DataFrame):
+    coords = df[["latitude", "longitude"]].values
 
-    db_scan = DBSCAN(
-        eps=EPS_KM,
-        min_samples=MIN_SAMPLES,
-        metric='precomputed'  # 告知 DBSCAN 使用我們已經計算好的距離矩陣
-    ).fit(km_dist_matrix)
+    model = AgglomerativeClustering(
+        n_clusters=None,
+        distance_threshold=0.005,  # 這裡的單位需視座標系而定
+        linkage="average",
+    )
 
-    df['Cluster_ID'] = db_scan.labels_
+    df["Cluster_ID"] = model.fit_predict(coords)
 
-    cluster_ids = sorted(df['Cluster_ID'].unique())
+    cluster_ids = sorted(df["Cluster_ID"].unique())
 
-    df['latitude'] = df['latitude'].astype(np.float64)
-    df['longitude'] = df['longitude'].astype(np.float64)
+    df["latitude"] = df["latitude"].astype(np.float64)
+    df["longitude"] = df["longitude"].astype(np.float64)
 
-    map_center_lat = df['latitude'].mean()
-    map_center_lon = df['longitude'].mean()
+    map_center_lat = df["latitude"].mean()
+    map_center_lon = df["longitude"].mean()
 
     colors = list(mcolors.TABLEAU_COLORS.keys())
     colormap = {}
     for i, c_id in enumerate(cluster_ids):
         colormap[c_id] = mcolors.TABLEAU_COLORS[colors[i % len(colors)]]
-    colormap[-1] = '#000000'  # 黑色
+    colormap[-1] = "#000000"  # 黑色
 
     m = folium.Map(
-        location=[
-            map_center_lat,
-            map_center_lon],
+        location=[map_center_lat, map_center_lon],
         zoom_start=10,
-        tiles='CartoDB Positron',
-        attr=f'Been Play 地點位置分析({EPS_KM=}, {MIN_SAMPLES=})',
-
+        tiles="CartoDB Positron",
+        attr=f"Been Play 地點位置分析({EPS_KM=}, {MIN_SAMPLES=})",
         # tiles=f'Been Play 地點位置分析({EPS_KM=}, {MIN_SAMPLES=})'
     )
     for idx, row in df.iterrows():
-        cluster_id = row['Cluster_ID']
-        lat = row['latitude']
-        lon = row['longitude']
+        cluster_id = row["Cluster_ID"]
+        lat = row["latitude"]
+        lon = row["longitude"]
         name = row["fullname"]
 
         point_color = colormap[cluster_id]
@@ -108,6 +105,7 @@ def dbscan(df: pd.DataFrame):
             <h3>{name}</h3>
             <p>經度: {lon:.4f}</p>
             <p>緯度: {lat:.4f}</p>
+            <p>Group: {cluster_id}</p>
         </div>
         """
         iframe = be.IFrame(html=popup_text, width="vw50", height="vh50")
@@ -122,7 +120,7 @@ def dbscan(df: pd.DataFrame):
             fill_opacity=0.9,
             popup=popup,  # 點擊時顯示資訊
         ).add_to(m)
-    output_file = DOCS_PATH / 'been_play_clusters_map.html'
+    output_file = DOCS_PATH / "been_play_agglomerative_clusters_map.html"
     m.save(output_file)
 
 
@@ -130,13 +128,12 @@ async def main():
     tasks = [
         get_json_files(file)
         for file in DATA_PATH.glob("*.json")
+        if not file.name.startswith("_")
     ]
     json_files = await asyncio.gather(*tasks)
 
     centers = clean_data([json_file["information"] for json_file in json_files])
-    with open("ADDRESS_GPS.json", "w") as f:
-        json.dump(centers, f)
-    dbscan(pd.DataFrame(centers))
+    hierarchical_clustering(pd.DataFrame(centers))
 
 
 if __name__ == "__main__":
