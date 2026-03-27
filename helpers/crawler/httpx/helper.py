cdd3397cb8857c2e222a7d198644fb1c63066711
JustIceQAQ <justiceqaq@gmail.com>
2026-03-27 12:48:45 +0800

    fix: HttpxAsyncClient __aenter__ and __aexit__ usage

--- helpers/crawler/httpx/helper.py
+++ helpers/crawler/httpx/helper.py
@@ -11,10 +11,18 @@ class HttpxAsyncClient:
     ) -> None:
         self.args = args
         self.kwargs = kwargs
-        self.client = httpx.AsyncClient(timeout=timeout, follow_redirects=follow_redirects, *args, **kwargs)
+        self.timeout = timeout
+        self.follow_redirects = follow_redirects
 
     async def __aenter__(self) -> httpx.AsyncClient:
+        self.client = httpx.AsyncClient(
+            timeout=self.timeout,
+            follow_redirects=self.follow_redirects,
+            *self.args,
+            **self.kwargs,
+        )
+        await self.client.__aenter__()
         return self.client
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.client.aclose()
+        await self.client.__aexit__(exc_type, exc_val, exc_tb)
