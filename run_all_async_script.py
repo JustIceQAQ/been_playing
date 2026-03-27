97e7b11c6584c150ba731c9fabac9ade3720844a
JustIceQAQ <justiceqaq@gmail.com>
2026-03-27 12:49:54 +0800

    fix: Semaphore usage

--- run_all_async_script.py
+++ run_all_async_script.py
@@ -60,6 +60,12 @@ async def generate_location(information: list["Information"]):
 
 
 async def main(worker: int | None = None, worker_max: int | None = None):
+    sem = asyncio.Semaphore(10)
+
+    async def run_with_sem(runner):
+        async with sem:
+            return await runner
+
     runtime_setting = get_settings()
 
     # logging init
@@ -95,11 +101,11 @@ async def main(worker: int | None = None, worker_max: int | None = None):
         this_runner = RunnerObj()
         all_script_information.append(this_runner.set_information())
         all_async_script_runners.append(RunnerObj().run(disk_cache, imgur, prefix))
-    async with asyncio.Semaphore(10):
-        await asyncio.gather(*all_async_script_runners, return_exceptions=True)
-        await generate_location(all_script_information)
 
-        await last_week_update.set_last_week_items()
+    await asyncio.gather(*[run_with_sem(r) for r in all_async_script_runners], return_exceptions=True)
+    await generate_location(all_script_information)
+
+    await last_week_update.set_last_week_items()
 
 
 if __name__ == "__main__":
