7af82b7d927576991e5ce10e0594cb31bd6e012c
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-10 12:49:38 +0800

    add: proxies

--- app/museums/tycg/script.py
+++ app/museums/tycg/script.py
@@ -2,6 +2,7 @@ import asyncio
 
 from typing import cast
 from app.museums.tycg.parse import TyCgParse
+from configs.settings import get_settings
 from helpers.headers_helper import generate_headers, generate_cookies
 from helpers.runner.helper import RunnerInit
 from helpers.storage.helper import Information, Coordinate
@@ -20,6 +21,16 @@ class TyCgRunner(RunnerInit):
     translation = SelectolaxTranslation
     use_parse = TyCgParse
     is_sort = False
+
+    def set_proxies(self):
+        runtime_settings = get_settings()
+        proxies = None
+        if runtime_settings.PROXY_POOL is not None:
+            proxies = {
+                "http": runtime_settings.PROXY_POOL,
+                "https": runtime_settings.PROXY_POOL,
+            }
+        return proxies
 
     def set_cache_expire(self) -> int | None:
         return month_3()
@@ -43,6 +54,8 @@ class TyCgRunner(RunnerInit):
             },
         )
         async with NiquestsAsyncSession(headers=headers, timeout=120) as client:
+            client.proxies.update(self.set_proxies())
+
             response = await client.get("https://wem.tycg.gov.tw/News_Photo.aspx?n=9676&sms=13653", cookies=cookies)
         return response.text
 
