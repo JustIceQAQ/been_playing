affb43b4f24d8bbc38ebd74516db07fb74dfe5c1
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-11 11:57:38 +0800

    fix: fetch_parsed select value

--- app/museums/ntaec/script.py
+++ app/museums/ntaec/script.py
@@ -27,20 +27,19 @@ class NTAECRunner(RunnerInit):
             location_code=Taiwan.taipei.zhongzheng_63000050,
             fullname="國立台灣藝術教育館",
             code_name="NTAEC",
-            external_link="https://www.arte.gov.tw/",
+            external_link="https://www.arte.gov.tw/pro1_exh_nowlist.asp",
             branch_coordinates=Coordinate(raw_coordinates="25.03249656295196, 121.51211159386773"),
             venue_type=VenueType.MUSEUM,
         )
 
     async def fetch_response(self):
         headers = generate_headers(need_upgrade_insecure_requests=True)
-        cookies = {"ASPSESSIONIDAUCTQQQB": "NEHFIKKCELKMMEJKIMGKIPDP"}
         page_no = 1
-        url = "https://www.arte.gov.tw/pro1_exh_nowlist.asp?PageNo=1"
+        url = "https://www.arte.gov.tw/pro1_exh_nowlist.asp"
         responses = []
         async with HttpxAsyncClient(headers=headers) as client:
             for n in range(3):
-                response = await client.get(url, params={"PageNo": page_no + n}, cookies=cookies)
+                response = await client.get(url, params={"PageNo": page_no + n})
                 responses.append(response.text)
         return responses
 
@@ -48,7 +47,7 @@ class NTAECRunner(RunnerInit):
         item_data = []
         parsed = cast(list[bs4.BeautifulSoup], await super().fetch_parsed())
         for p in parsed:
-            items = p.select("div.user-postes.wow")
+            items = p.select("div.single-page div.user-postes div.row")
             item_data.extend(items)
         return item_data
 
