b39f06b96bd373f6b2679df35c846abb41fdafb9
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-23 09:30:48 +0800

    add: NWACRunner

---
+++ app/galleries/nwac/parse.py
@@ -0,0 +1,27 @@
+from helpers.parse_helper import ParseInit
+
+
+from selectolax.lexbor import LexborNode
+
+
+class NWACParse(ParseInit):
+    def __init__(self, item: LexborNode):
+        self.item = item
+
+    def get_title(self, *args, **kwargs) -> str | None:
+        return self.item.css_first("img").attributes.get("alt")
+
+    def get_date(self, *args, **kwargs) -> str | None:
+        pass
+
+    def get_address(self, *args, **kwargs) -> str | None:
+        pass
+
+    def get_figure(self, *args, **kwargs) -> str | None:
+        return self.item.css_first("img").attributes.get("src")
+
+    def get_tags(self, *args, **kwargs) -> list[str | None] | None:
+        pass
+
+    def get_source_url(self, *args, **kwargs) -> str | None:
+        return self.item.css_first("a").attributes.get("href")
