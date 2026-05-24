275a307655b2123a0f431f7b7462371f20922faa
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-05-25 01:08:05 +0800

    add: get_information and social_media

---
+++ app/museums/shungyeart/social_media.py
@@ -0,0 +1,11 @@
+from helpers.storage.social_media import SocialMedia
+
+
+class ShungYeArtSocialMedia:
+    @staticmethod
+    def get_social_media() -> SocialMedia:
+        return SocialMedia(
+            facebook="https://www.facebook.com/%E9%A0%86%E7%9B%8A%E5%8F%B0%E7%81%A3%E7%BE%8E%E8%A1%93%E9%A4%A8-109351457469127/",
+            instagram="https://www.instagram.com/symfa_tw/",
+            youtube="https://www.youtube.com/@%E9%A0%86%E7%9B%8A%E5%8F%B0%E7%81%A3%E7%BE%8E%E8%A1%93%E9%A4%A8",
+        )
