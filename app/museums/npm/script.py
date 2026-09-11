77ce2dff91328287861368dd211717452d371536
JustIceQAQ <49470040+JustIceQAQ@users.noreply.github.com>
2026-09-11 19:52:36 +0800

    fix: use SSL_CONTEXT

--- app/museums/npm/script.py
+++ app/museums/npm/script.py
@@ -81,7 +81,7 @@ class NpmRunner(RunnerInit):
             need_upgrade_insecure_requests=True,
             host="www.npm.gov.tw",
         )
-        async with HttpxAsyncClient(headers=this_header, use_proxy=True) as client:
+        async with HttpxAsyncClient(headers=this_header, use_proxy=False) as client:
             results = await asyncio.gather(
                 *[
                     client.get(
